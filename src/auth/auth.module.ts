39f8cfcd1384ff07a197949a52700c29dfa62828
mahnoor faisal <faisalmahnoor86@gmail.com>
2024-06-21 16:16:15 +0500

    auth in progress

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-    imports : [PassportModule]
+    imports : [PassportModule,UsersModule],
+    providers: [LocalStrategy]
 })
 export class AuthModule {}
--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,4 +1,23 @@
 import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from 'passport-local';
+import { UsersService } from "src/users/users.service";
+import { User } from "src/users/users.model";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 
+@Injectable()
+ export class LocalStrategy extends PassportStrategy(Strategy) {
 
-// export class LocalStrategy extends PassportStrategy(Strategy) {}
+    constructor(private readonly userService: UsersService){
+        super();                                                 //super() refers to the extended class
+    }
+    validate(username:string , password:string): User{
+            const user :User = this.userService.getUserByUserName(username);
+            if(user===undefined) throw new UnauthorizedException();
+            if(user!=undefined && user.password===password){
+                return user;
+            }
+            else {
+                throw new UnauthorizedException();
+            }
+    }
+ }
